62322c78239755ede8861ed396bf793b4755b741
Axel Rindle <mail@axelrindle.de>
2024-05-31 21:32:26 +0200

    refactor: remove /api prefix
    Laravel serves as an api as a whole, so the prefix is unnecessary

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,12 +8,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
+
+        apiPrefix: '',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->validateCsrfTokens(except: [
-            'api/*'
-        ]);
-
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
