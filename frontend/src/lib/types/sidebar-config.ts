2b76371d224196f6ae741626af1e13ce0d3a14df
Christopher Werl <christopher.werl@adesso.de>
2024-06-09 17:33:25 +0200

    refactor: Delete SidebarConfig type

--- frontend/src/lib/types/sidebar-config.ts
+++ frontend/src/lib/types/sidebar-config.ts
@@ -1,13 +1,5 @@
-import { LucideIcon, LucideProps } from "lucide-react";
-import { ReactNode } from "react";
-import dynamicIconImports from "lucide-react/dynamicIconImports";
+import { LucideProps } from "lucide-react";
 import React from "react";
-
-export type SidebarConfig = {
-    breakpoint?: string;
-    links: SidebarLink[];
-    extras?: ReactNode;
-};
 
 export interface SidebarLink {
     label: string;
