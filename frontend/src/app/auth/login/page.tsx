0c8a2bc56341abd497acc6c5d265087c953a6f14
Axel Rindle <mail@axelrindle.de>
2024-06-26 16:14:00 +0200

    feat: add back button to login page

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,6 +1,9 @@
 import { getAppSession } from "@/app/actions";
 import LoginForm from "@/lib/components/auth/LoginForm";
+import { Button } from "@/lib/components/common/Button";
+import { ArrowLeft } from "lucide-react";
 import { getTranslations } from "next-intl/server";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function Login() {
@@ -15,11 +18,24 @@ export default async function Login() {
     return (
         <>
             <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-b from-primary/10">
-                <div className="max-w-[450px] w-full p-5">
+                <div className="max-w-[450px] w-full p-5 space-y-5">
                     <h1 className="text-3xl font-semibold mb-5">
                         Welcome back.
                     </h1>
+
                     <LoginForm />
+
+                    <Button
+                        type="submit"
+                        className="w-full"
+                        variant="outline"
+                        asChild
+                    >
+                        <Link href="/">
+                            <ArrowLeft />
+                            <span className="ml-2">Zurück</span>
+                        </Link>
+                    </Button>
                 </div>
             </div>
         </>
