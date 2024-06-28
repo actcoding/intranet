f6962993fa55e3958088a867ca82da9a70f4ef03
Axel Rindle <mail@axelrindle.de>
2024-06-28 20:08:35 +0200

    chore: update db seeding

---
+++ database/seeders/Policy/DatabaseSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders\Policy;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $role = Role::firstOrCreate([
+            'name' => 'Creator',
+        ]);
+
+        $this->call(
+            class: [
+                NewsSeeder::class,
+            ],
+            parameters: [
+                $role,
+            ],
+        );
+
+        $role->givePermissionTo(Permission::all());
+
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+    }
+}
---
+++ database/seeders/Policy/NewsSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders\Policy;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class NewsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Role $role): void
+    {
+        $names = [
+            'news.viewall',
+            'news.create',
+            'news.update',
+            'news.delete',
+            'news.restore',
+            'news.delete.force',
+        ];
+
+        foreach ($names as $name) {
+            Permission::firstOrCreate([
+                'name' => $name,
+            ]);
+        }
+    }
+}
