23dac87312a2ae13eb683519395aa2a9eb3b7abd
Axel Rindle <mail@axelrindle.de>
2024-07-01 15:41:22 +0200

    feat: add perPage parameter

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -7,8 +7,12 @@ use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
+    /**
+     * @LRDparam page integer|min:1
+     * @LRDparam perPage integer|min:1|default:10
+     */
     public function list(Request $request)
     {
-        return News::query()->paginate();
+        return News::query()->simplePaginate($request->query('perPage', 10));
     }
 }
