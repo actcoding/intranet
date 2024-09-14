0dc793f4675816cd29febaf24e7826a65ad7d39a
Axel Rindle <axel.rindle@act-coding.de>
2024-09-14 11:06:54 +0200

    fix translations

--- frontend/src/lib/components/news/news-list/NewsList.tsx
+++ frontend/src/lib/components/news/news-list/NewsList.tsx
@@ -1,12 +1,16 @@
 import { newsApi } from '@/lib/api/api'
 import NewsListContent from '@/lib/components/news/news-list/components/NewsListContent'
+import { pick } from 'lodash'
 import { NextIntlClientProvider } from 'next-intl'
+import { getMessages } from 'next-intl/server'
 
 const NewsList = async () => {
     const news = await newsApi.newsIndex({ page: 1, perPage: 6 })
+    const messages = await getMessages()
+
     if (news.data) {
         return (
-            <NextIntlClientProvider>
+            <NextIntlClientProvider messages={pick(messages, 'News')}>
                 <NewsListContent initialNews={news.data} />
             </NextIntlClientProvider>
         )
