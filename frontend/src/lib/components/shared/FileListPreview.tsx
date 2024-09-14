8c961278ac0b5f78f21178b5c2ba06e3093e2d13
Axel Rindle <axel.rindle@act-coding.de>
2024-09-14 10:47:03 +0200

    update preview

--- frontend/src/lib/components/shared/FileListPreview.tsx
+++ frontend/src/lib/components/shared/FileListPreview.tsx
@@ -12,7 +12,7 @@ interface FileListPreviewProps {
     files: File[] | AttachmentResourceData[];
     display?: 'list' | 'grid';
     download?: boolean
-    onRemove?: (file: File) => void;
+    onRemove?: (file: File | AttachmentResourceData) => void;
 }
 
 const FileListPreview = ({
@@ -35,22 +35,25 @@ const FileListPreview = ({
                     >
                         <Card
                             className={cn(
-                                'flex items-center gap-2 me-2 mb-2 px-2',
-                                download && 'hover:bg-muted cursor-pointer',
+                                'flex flex-row items-center gap-x-2 mb-2 px-2 group',
                                 props.onRemove && 'pr-0',
                             )}
-                            onClick={() => window.open(file.url, '_blank')}
                         >
                             <FileTypeIcon
                                 fileType={file.type ?? 'text/plain'}
                                 size={16}
                                 className="shrink-0"
                             />
-                            <span className="line-clamp-1 py-2">
+                            <span className="line-clamp-1 flex-1 py-2">
                                 {file.name}
                             </span>
-                            {download ? <DownloadIcon /> : null}
-                            {props.onRemove === undefined ? null : (
+                            {download ? (
+                                <DownloadIcon
+                                    onClick={() => window.open(file.url, '_blank')}
+                                    className='cursor-pointer'
+                                />
+                            ) : null}
+                            {props.onRemove !== undefined ? (
                                 <Button
                                     type="button"
                                     variant={'ghost'}
@@ -65,7 +68,7 @@ const FileListPreview = ({
                                     />
                                     <span className="sr-only">Remove</span>
                                 </Button>
-                            )}
+                            ) : null}
                         </Card>
                     </motion.li>
                 ))}
