87561adbc0cb3a62c574cb20936d65b88af077e1
Thomas Petersen <thomas.petersen@act-coding.de>
2024-12-11 00:02:36 +0100

    fix

--- frontend/src/lib/components/manage/manage-events/manage-events-table/ManageEventsTable.config.tsx
+++ frontend/src/lib/components/manage/manage-events/manage-events-table/ManageEventsTable.config.tsx
@@ -53,13 +53,13 @@ function CellActions({ row }: {row: Row<EventResource>}) {
                     </DropdownMenuItem>
                 ) : null}
                 <DropdownMenuItem asChild>
-                    <Link href={`/event/${event.id}`}>
+                    <Link href={`/events/${event.id}`}>
                         <EyeIcon size={16} className="mr-2" />
                         Vorschau
                     </Link>
                 </DropdownMenuItem>
                 <DropdownMenuItem asChild>
-                    <Link href={`/manage/event/${event.id}`}>
+                    <Link href={`/manage/events/${event.id}`}>
                         <Edit2Icon size={16} className="mr-2" />
                         Bearbeiten
                     </Link>
