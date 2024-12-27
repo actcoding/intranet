01273a79f475c9998fff4ab01a18776528833388
Christopher Werl <christopher.werl@adesso.de>
2024-12-27 17:34:52 +0100

    feat: Update sidebar and create links for menu stuff

--- frontend/src/app/(shell)/layout.tsx
+++ frontend/src/app/(shell)/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/globals.css'
-import { Sidebar } from '@/core/layout/Sidebar'
+import {Sidebar, SidebarProvider, SidebarTrigger} from '@/core/layout/Sidebar'
+import {cookies} from 'next/headers'
 import React from 'react'
 
 export default async function ShellLayout({
@@ -7,12 +8,16 @@ export default async function ShellLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const cookieStore = cookies()
+    const defaultOpen = cookieStore.get('sidebar:state')?.value === 'true'
+
     return (
-        <>
+        <SidebarProvider defaultOpen={defaultOpen}>
             <Sidebar />
-            <main className="mx-5 mt-16 h-screen sm:ml-[300px] sm:mt-3">
+            <main className="mx-5 mt-16 h-screen w-full sm:mt-3">
+                <SidebarTrigger />
                 {children}
             </main>
-        </>
+        </SidebarProvider>
     )
 }
