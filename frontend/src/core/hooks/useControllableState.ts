e5e1c7b1a1efd57656f43f226696cc7de6ff6624
Axel Rindle <axel.rindle@act-coding.de>
2025-02-08 13:51:53 +0000

    fix: move type guard out of scope
    plus added some documentation

--- frontend/src/core/hooks/useControllableState.ts
+++ frontend/src/core/hooks/useControllableState.ts
@@ -1,5 +1,31 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 
+/**
+ * Defines a "controlled" value as a "defined" value,
+ * thus a value which is neither `null` nor `undefined`.
+ *
+ * @param value The value to check whether it is defined or not.
+ * @returns Whether the given value is defined or not.
+ */
+function isControlled<T>(value: T | undefined): value is T {
+    return value !== undefined
+}
+
+/**
+ * Hook to manage controllable state in React components.
+ *
+ * This hook allows you to create a state that can be controlled by a parent component
+ * through the `controlledValue` prop, or uncontrolled, where the state is managed
+ * internally by the component.
+ *
+ * @param controlledValue The controlled value of the state, or `undefined` if uncontrolled.
+ * @param defaultValue The default value of the state if it's uncontrolled.
+ *
+ * @returns An array containing:
+ *  - The current value of the state.
+ *  - A function to update the state, only applicable when the state is uncontrolled.
+ *  - A boolean indicating whether the state is controlled or not.
+ */
 export function useControllableState<T>(
     controlledValue: T | undefined,
     defaultValue: T,
@@ -7,10 +33,6 @@ export function useControllableState<T>(
     const [internalValue, setInternalValue] = useState(
         controlledValue ?? defaultValue,
     )
-
-    const isControlled = (value: T | undefined): value is T => {
-        return value !== undefined
-    }
 
     useEffect(() => {
         if (isControlled(controlledValue)) {
